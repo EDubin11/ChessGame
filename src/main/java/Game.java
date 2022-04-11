53f0bf5f4ec9502be905514812155cfd21586882
Abraham Linzer <alinzer@mail.yu.edu>
2022-04-11 17:26:56 -0400

    j

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,5 +5,26 @@ public class Game {
         this.board = new Piece[8][8];
     }
     
+    public void move(String move) {
+        String start = move.substring(0,2);
+        String end = move.substring(6);
+        int startLetter = (int)start.charAt(0) - 65;
+        Character startNumChar = start.charAt(1);
+        int startNumber = Character.getNumericValue(startNumChar);
+        int endLetter = (int)end.charAt(0) - 65;
+        Character endNumChar = end.charAt(1);
+        int endNumber = Character.getNumericValue(endNumChar);
+        
+        Piece pieceToMove = this.board[startLetter][startNumber];
+        while (!this.validMove(startLetter, startNumber, endLetter, endNumber) {
+            
+        }
+    }
+    
+    public boolean validMove(int startLetter, int startNumber, int endLetter, int endNumber) {
+        switch (this.board[startLetter][startNumber]) {
+            case 
+        }
+    }
     
 }
