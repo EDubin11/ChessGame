5b867c128f63d38ab6c3cf9c4a7285a5b23bf6f3
Ethan <edubin@mail.yu.edu>
2022-04-13 18:01:41 -0400

    y

--- src/main/java/Piece.java
+++ src/main/java/Piece.java
@@ -1,10 +1,11 @@
 public abstract class Piece {
+    private Color color;
     boolean differentColor(Piece other){
         return other.getColor() == other.getColor();
     }
     abstract boolean validMoving(String start, String end);
     Color getColor(){
-        return this.getColor();
+        return this.color;
     }
     String getType(){
         return this.getClass().toString();
